1d7d4323ade7094f984f3f6f8479e0148aa6213d
Prince Moses <princyunlimited@gmail.com>
2023-02-20 07:23:53 -0800

    Hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int fig;
+	char alp;
+
+	for (fig = 0; fig <= 9; fig++)
+	{
+		putchar(fig);
+	}
+	for (alp = 'a'; alp <= 'f'; alp++)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
